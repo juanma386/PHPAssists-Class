52fd3db9a043d086a8122058bfaec2620796e444
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2023-11-26 23:54:00 -0300

    Update HelperMain.php

--- src/Shared/HelperMain.php
+++ src/Shared/HelperMain.php
@@ -8,14 +8,37 @@ class HelperMain {
         $namespace = 'Helpers\\'; // Namespace base
         $baseDir = __DIR__ . '/'; // Directorio base
 
-        // Convertir separadores de namespace en separadores de directorio
         $className = str_replace($namespace, '', $className);
         $filePath = $baseDir . str_replace('\\', '/', $className) . '.php';
 
-        // Verificar si el archivo existe antes de incluirlo
+        // Verificar si el archivo existe en el directorio base
         if (file_exists($filePath)) {
             require_once $filePath;
+            return;
+        }
+
+        // Buscar recursivamente en subdirectorios
+        $this->autoloadInSubdirectories($baseDir, $className);
+    }
+
+    private function autoloadInSubdirectories($directory, $className) {
+        $directoryIterator = new RecursiveDirectoryIterator($directory);
+        $iterator = new RecursiveIteratorIterator($directoryIterator);
+        
+        foreach ($iterator as $file) {
+            if ($file->isDir()) {
+                continue; // Ignorar si es un directorio
+            }
+            
+            $filePath = $file->getPathname();
+            $filePathWithoutBase = str_replace(__DIR__ . '/', '', $filePath);
+
+            if (strpos($filePathWithoutBase, $className . '.php') !== false) {
+                require_once $filePath;
+                return;
+            }
         }
     }
 }
 ?>
+
