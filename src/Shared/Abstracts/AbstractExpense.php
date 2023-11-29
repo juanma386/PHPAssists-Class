263b26ec8b9e6705af447f3ec865ef1c1e5bea19
Juan Manuel <xomie_01@hotmail.com>
2023-11-29 18:53:29 -0300

    Update fix

--- src/Shared/Abstracts/AbstractExpense.php
+++ src/Shared/Abstracts/AbstractExpense.php
@@ -6,7 +6,7 @@ use PHPAssists\Shared\Core\ClassInvocationProcessor;
 /**
  * PHPAssists Expense Abstract
  *
- * This class represents individual expense abstract within the PHPAssists framework.
+ * This class represents individual expense within the PHPAssists framework.
  * It manages information related to expenses.
  *
  * @link       https://hexome.com.ar
@@ -72,88 +72,6 @@ class AbstractExpense extends ClassInvocationProcessor {
         $this->setExpenseDate((string) $expense_date);
     }
 
-    // Getters
-
-    /**
-     * Retrieves the unique identifier for the expense entity.
-     *
-     * @return ?string The expense identifier or null if not set.
-     */
-    abstract public function getId() : ?string;
-
-    /**
-     * Retrieves the category identifier for the expense.
-     *
-     * @return ?string The category identifier or null if not set.
-     */
-    abstract public function getCategoryId() : ?string;
-
-    /**
-     * Retrieves the description of the expense.
-     *
-     * @return ?string The expense description or null if not set.
-     */
-    abstract public function getDescription() : ?string;
-
-    /**
-     * Retrieves the amount of the expense.
-     *
-     * @return float|null The expense amount or null if not set.
-     */
-    abstract public function getAmount() : ? float;
-
-    /**
-     * Retrieves the date of the expense.
-     *
-     * @return ?string The expense date or null if not set.
-     */
-    abstract public function getExpenseDate() : ?string;
-
-    // Setters
-
-    /**
-     * Sets the unique identifier for the expense entity.
-     *
-     * @param ?string $id The expense identifier.
-     *
-     * @return void
-     */
-    abstract public function setId(?string $id): void;
-
-    /**
-     * Sets the category identifier for the expense.
-     *
-     * @param ?string $category_id The category identifier.
-     *
-     * @return void
-     */
-    abstract public function setCategoryId(?string $category_id): void;
-
-    /**
-     * Sets the description of the expense.
-     *
-     * @param ?string $description The expense description.
-     *
-     * @return void
-     */
-    abstract public function setDescription(?string $description): void;
-
-    /**
-     * Sets the amount of the expense.
-     *
-     * @param ?float $amount The expense amount.
-     *
-     * @return void
-     */
-    abstract public function setAmount(?float $amount): void;
-
-    /**
-     * Sets the date of the expense.
-     *
-     * @param ?string $expense_date The expense date.
-     *
-     * @return void
-     */
-    abstract public function setExpenseDate(?string $expense_date): void;
+  
 }
 ?>
