4cedc4d19e9f136901de686c98d1fe23ae15145c
Juan Manuel <xomie_01@hotmail.com>
2023-11-30 17:17:34 -0300

    added functions an others constructor isset Response Library

--- src/Shared/Core/Response/Abstracts/AbstractBaseResponse.php
+++ src/Shared/Core/Response/Abstracts/AbstractBaseResponse.php
@@ -56,48 +56,16 @@ abstract class AbstractBaseResponse {
     protected static int $VERSION_MISMATCH = self::VERSION_MISMATCH;
     protected static int $BRANWIDTH_LIMIT_EXCEEDED = self::BRANWIDTH_LIMIT_EXCEEDED;
 
-    protected static array $POSITIVE_CODES_STATUS = [
-        self::$SUCCESS,
-        self::$CREATED,
-        self::$ACCEPTED,
-        self::$MOVED_PERMANENTLY,
-        self::$TEMPORARY_REDIRECT,
-        self::$PERMANENT_REDIRECT,
-    ];
+    protected static array $POSITIVE_CODES_STATUS = [];
+    protected static array $RESPONSE_CODES_STATUS = [];
 
-    protected static array $RESPONSE_CODES_STATUS = [
-        self::$SUCCESS,
-        self::$CREATED,
-        self::$ACCEPTED,
-        self::$MOVED_PERMANENTLY,
-        self::$TEMPORARY_REDIRECT,
-        self::$PERMANENT_REDIRECT,
-        self::$BAD_REQUEST,
-        self::$NOTAUTH,
-        self::$PAYMENT_REQUIRED,
-        self::$FORBIDDEN,
-        self::$NOTFOUND,
-        self::$NOTALLOWED,
-        self::$REQUEST_TIMEOUT,
-        self::$CONFLICT,
-        self::$UNPROCESSABLE,
-        self::$SERVERERROR,
-        self::$GONE,
-        self::$TEAPOT,
-        self::$FAILED_DEPENDENCY,
-        self::$UPGRADE_REQUIRED,
-        self::$UNAVAILABLE_OF_LEGAL,
-        self::$NOT_IMPLEMENTED,
-        self::$SERVICE_UNAVAILABLE,
-        self::$GATEWAY_UNAVAILABLE,
-        self::$NETWORK_AUTH_REQUIRED,
-        self::$NOT_UPDATED,
-        self::$VERSION_MISMATCH,
-        self::$BRANWIDTH_LIMIT_EXCEEDED,
-    ];
+    public function __construct()
+    {
+        self::$POSITIVE_CODES_STATUS = self::getPositiveCodesStatus();
+        self::$RESPONSE_CODES_STATUS = self::isResponseCodesStatus();
+    }
 
     public static function setResponse(?int $response_code, $data = null) {
-        
         $response = new \HTTPEntity((int) $response_code, $data);
         $response->status = self::isResponse($response_code) ? $response_code : self::$NOT_IMPLEMENTED;
         self::isPositiveResponse($response_code) ? [ $response->data = $data ] : [ $response->error = $data ];
@@ -110,6 +78,52 @@ abstract class AbstractBaseResponse {
 
     public static function isResponse(?int $response_code) {
         return in_array($response_code, self::$RESPONSE_CODES_STATUS);
+    }
+
+    protected static function getPositiveCodesStatus(): array
+    {
+        return [
+            self::$SUCCESS,
+            self::$CREATED,
+            self::$ACCEPTED,
+            self::$MOVED_PERMANENTLY,
+            self::$TEMPORARY_REDIRECT,
+            self::$PERMANENT_REDIRECT,
+        ];
+    }
+
+    protected static function isResponseCodesStatus(): array
+    {
+        return [
+            self::$SUCCESS,
+            self::$CREATED,
+            self::$ACCEPTED,
+            self::$MOVED_PERMANENTLY,
+            self::$TEMPORARY_REDIRECT,
+            self::$PERMANENT_REDIRECT,
+            self::$BAD_REQUEST,
+            self::$NOTAUTH,
+            self::$PAYMENT_REQUIRED,
+            self::$FORBIDDEN,
+            self::$NOTFOUND,
+            self::$NOTALLOWED,
+            self::$REQUEST_TIMEOUT,
+            self::$CONFLICT,
+            self::$UNPROCESSABLE,
+            self::$SERVERERROR,
+            self::$GONE,
+            self::$TEAPOT,
+            self::$FAILED_DEPENDENCY,
+            self::$UPGRADE_REQUIRED,
+            self::$UNAVAILABLE_OF_LEGAL,
+            self::$NOT_IMPLEMENTED,
+            self::$SERVICE_UNAVAILABLE,
+            self::$GATEWAY_UNAVAILABLE,
+            self::$NETWORK_AUTH_REQUIRED,
+            self::$NOT_UPDATED,
+            self::$VERSION_MISMATCH,
+            self::$BRANWIDTH_LIMIT_EXCEEDED,
+        ];
     }
 
     abstract public static function ajaxResponse(?int $response_code, $data = null, $header = false) : string;
