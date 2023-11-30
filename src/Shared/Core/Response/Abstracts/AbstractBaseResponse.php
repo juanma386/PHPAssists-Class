e3a25299e2b0473f66cb3976940e3a9cd10176ae
Juan Manuel <xomie_01@hotmail.com>
2023-11-30 18:06:31 -0300

    new

--- src/Shared/Core/Response/Abstracts/AbstractBaseResponse.php
+++ src/Shared/Core/Response/Abstracts/AbstractBaseResponse.php
@@ -10,55 +10,13 @@ namespace PHPAssists\Shared\Core\Response\Abstracts;
  * @since      1.0.0
  *
  * @package    PHPAssists
- * @subpackage PHPAssists\Shared\Core\Response\Traits
+ * @subpackage PHPAssists\Shared\Core\Response\Abstracts
  *
  * @author     Hexome Cloud <hi@hexome.cloud>
  */
-
-use PHPAssists\Shared\Core\Response\Traits\TraitHttpResponse;
 use PHPAssists\Shared\Core\Response\Entities\HTTPEntity;
-use PHPAssists\Shared\Core\Response\Interfaces\InterfaceHttpResponseCodes;
 
-abstract class AbstractBaseResponse implements InterfaceHttpResponseCodes  {
-
-    use TraitHttpResponse;
-
-    protected static int $SUCCESS = self::SUCCESS;
-    protected static int $CREATED = self::CREATED;
-    protected static int $ACCEPTED = self::ACCEPTED;
-    protected static int $MOVED_PERMANENTLY = self::MOVED_PERMANENTLY;
-    protected static int $TEMPORARY_REDIRECT = self::TEMPORARY_REDIRECT;
-    protected static int $PERMANENT_REDIRECT = self::PERMANENT_REDIRECT;
-
-    protected static int $BAD_REQUEST = self::BAD_REQUEST;
-    protected static int $UNAUTHORIZED = self::UNAUTHORIZED;
-    protected static int $PAYMENT_REQUIRED = self::PAYMENT_REQUIRED;
-
-    protected static int $FORBIDDEN = self::FORBIDDEN;
-    protected static int $NOT_FOUND = self::NOT_FOUND;
-    protected static int $NOT_ALLOWED = self::NOT_ALLOWED;
-    protected static int $REQUEST_TIMEOUT = self::REQUEST_TIMEOUT;
-    protected static int $CONFLICT = self::CONFLICT;
-    protected static int $UNPROCESSABLE_ENTITY = self::UNPROCESSABLE_ENTITY;
-    protected static int $INTERNAL_SERVER_ERROR = self::INTERNAL_SERVER_ERROR;
-
-    protected static int $GONE = self::GONE;
-    protected static int $TEAPOT = self::TEAPOT;
-    protected static int $FAILED_DEPENDENCY = self::FAILED_DEPENDENCY;
-    protected static int $UPGRADE_REQUIRED = self::UPGRADE_REQUIRED;
-
-    protected static int $UNAVAILABLE_FOR_LEGAL = self::UNAVAILABLE_FOR_LEGAL;
-    protected static int $NOT_IMPLEMENTED = self::NOT_IMPLEMENTED;
-    protected static int $SERVICE_UNAVAILABLE = self::SERVICE_UNAVAILABLE;
-    protected static int $GATEWAY_TIMEOUT = self::GATEWAY_TIMEOUT;
-
-    protected static int $NETWORK_AUTH_REQUIRED = self::NETWORK_AUTH_REQUIRED;
-    protected static int $NOT_UPDATED = self::NOT_UPDATED;
-    protected static int $VERSION_MISMATCH = self::VERSION_MISMATCH;
-    protected static int $BANDWIDTH_LIMIT_EXCEEDED = self::BANDWIDTH_LIMIT_EXCEEDED;
-
-    protected static array $POSITIVE_CODES_STATUS = [];
-    protected static array $RESPONSE_CODES_STATUS = [];
+abstract class AbstractBaseResponse extends HTTPEntity {
 
     public function __construct()
     {
@@ -67,7 +25,7 @@ abstract class AbstractBaseResponse implements InterfaceHttpResponseCodes  {
     }
 
     public static function setResponse(?int $response_code, $data = null) {
-        $response = new HTTPEntity((int) $response_code, $data);
+        $response = new self((int) $response_code, $data);
         $response->status = self::isResponse($response_code) ? $response_code : self::$NOT_IMPLEMENTED;
         self::isPositiveResponse($response_code) ? [ $response->data = $data ] : [ $response->error = $data ];
         return $response;
--- src/Shared/Core/Response/Entities/HTTPEntity.php
+++ src/Shared/Core/Response/Entities/HTTPEntity.php
@@ -19,6 +19,44 @@ use PHPAssists\Shared\Core\Response\Interfaces\InterfaceHttpResponseCodes;
 
 class HTTPEntity implements InterfaceHttpResponseCodes {
 
+
+    protected static int $SUCCESS = self::SUCCESS;
+    protected static int $CREATED = self::CREATED;
+    protected static int $ACCEPTED = self::ACCEPTED;
+    protected static int $MOVED_PERMANENTLY = self::MOVED_PERMANENTLY;
+    protected static int $TEMPORARY_REDIRECT = self::TEMPORARY_REDIRECT;
+    protected static int $PERMANENT_REDIRECT = self::PERMANENT_REDIRECT;
+
+    protected static int $BAD_REQUEST = self::BAD_REQUEST;
+    protected static int $UNAUTHORIZED = self::UNAUTHORIZED;
+    protected static int $PAYMENT_REQUIRED = self::PAYMENT_REQUIRED;
+
+    protected static int $FORBIDDEN = self::FORBIDDEN;
+    protected static int $NOT_FOUND = self::NOT_FOUND;
+    protected static int $NOT_ALLOWED = self::NOT_ALLOWED;
+    protected static int $REQUEST_TIMEOUT = self::REQUEST_TIMEOUT;
+    protected static int $CONFLICT = self::CONFLICT;
+    protected static int $UNPROCESSABLE_ENTITY = self::UNPROCESSABLE_ENTITY;
+    protected static int $INTERNAL_SERVER_ERROR = self::INTERNAL_SERVER_ERROR;
+
+    protected static int $GONE = self::GONE;
+    protected static int $TEAPOT = self::TEAPOT;
+    protected static int $FAILED_DEPENDENCY = self::FAILED_DEPENDENCY;
+    protected static int $UPGRADE_REQUIRED = self::UPGRADE_REQUIRED;
+
+    protected static int $UNAVAILABLE_FOR_LEGAL = self::UNAVAILABLE_FOR_LEGAL;
+    protected static int $NOT_IMPLEMENTED = self::NOT_IMPLEMENTED;
+    protected static int $SERVICE_UNAVAILABLE = self::SERVICE_UNAVAILABLE;
+    protected static int $GATEWAY_TIMEOUT = self::GATEWAY_TIMEOUT;
+
+    protected static int $NETWORK_AUTH_REQUIRED = self::NETWORK_AUTH_REQUIRED;
+    protected static int $NOT_UPDATED = self::NOT_UPDATED;
+    protected static int $VERSION_MISMATCH = self::VERSION_MISMATCH;
+    protected static int $BANDWIDTH_LIMIT_EXCEEDED = self::BANDWIDTH_LIMIT_EXCEEDED;
+
+    protected static array $POSITIVE_CODES_STATUS = [];
+    protected static array $RESPONSE_CODES_STATUS = [];
+
      /**
      * @var int Status code for the response.
      */
@@ -77,5 +115,5 @@ class HTTPEntity implements InterfaceHttpResponseCodes {
         return true; // Add your validation logic here
     }
 
-    
+
 }
