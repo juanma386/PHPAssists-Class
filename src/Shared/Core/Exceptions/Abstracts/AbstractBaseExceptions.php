7d4d365396e504fadd77b3914895417eabbbd04f
Juan Manuel <xomie_01@hotmail.com>
2023-12-08 00:25:25 -0300

    Issets comments

--- src/Shared/Core/Exceptions/Abstracts/AbstractBaseExceptions.php
+++ src/Shared/Core/Exceptions/Abstracts/AbstractBaseExceptions.php
@@ -1,6 +1,18 @@
 <?php
+/**
+ * PHPAssists Exceptions API
+ *
+ * This class defines the possible Functions for the PHPAssists Exceptions API.
+ *
+ * @link       https://hexome.com.ar
+ * @since      0.0.4
+ *
+ * @package    PHPAssists
+ * @subpackage PHPAssists\Shared\Core\Exceptions\Abstracts
+ *
+ * @author     Hexome Cloud <hi@hexome.cloud>
+ */
 namespace PHPAssists\Shared\Core\Exceptions\Abstracts;
-
 abstract class AbstractBaseExceptions extends Exception {
     abstract public function log(?string $message) : ?string;
     public function errorMessage() : ?string {
--- src/Shared/Core/Exceptions/BaseExceptions.php
+++ src/Shared/Core/Exceptions/BaseExceptions.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * PHPAssists Exceptions API
+ *
+ * This class defines the possible Functions for the PHPAssists Exceptions API.
+ *
+ * @link       https://hexome.com.ar
+ * @since      0.0.4
+ *
+ * @package    PHPAssists
+ * @subpackage PHPAssists\Shared\Core\Exceptions
+ *
+ * @author     Hexome Cloud <hi@hexome.cloud>
+ */
 namespace PHPAssists\Shared\Core\Exceptions;
 use namespace PHPAssists\Shared\Core\Exceptions\Abstracts\AbstractBaseExceptions;
 class BaseException extends AbstractBaseExceptions {}
