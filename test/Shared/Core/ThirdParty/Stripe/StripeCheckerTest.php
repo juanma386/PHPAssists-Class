ca40504ad6e9c89e6cb5f10acf41127608ae277e
Juan Manuel <xomie_01@hotmail.com>
2023-12-11 20:13:22 -0300

    Update testing to Stripe ThirdParty

---
+++ test/Shared/Core/ThirdParty/Stripe/StripeCheckerTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace PHPAssistsTest\Shared\Core\ThirdParty\Stripe;
+/**
+ * PHPAssists Stripe API Test
+ *
+ * This class defines the possible Functions for the PHPAssists Stripe API Test.
+ *
+ * @link       https://hexome.com.ar
+ * @since      0.0.1
+ *
+ * @package    PHPAssistsTest
+ * @subpackage PHPAssistsTest\Shared\Core\ThirdParty\Stripe
+ *
+ * @author     Hexome Cloud <hi@hexome.cloud>
+ */
+
+use PHPUnit\Framework\TestCase;
+use PHPAssists\Shared\Core\ThirdParty\Stripe\StripeChecker;
+
+class StripeCheckerTest extends TestCase
+{
+    private $stripe;
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->stripe = new StripeChecker();
+    }
+
+    public function testClassExists() : void
+    {
+        $this->assertInstanceOf(StripeChecker::class, $this->stripe);
+    }
+    public function testCheckStripe() : void {
+        $status = $this->stripe->check();
+        $this->assertTrue($status);
+    }
+}
+?>
