84b475612696d5d22cb9594ed5da1307b80ab075
Juan Manuel <xomie_01@hotmail.com>
2023-12-11 23:06:36 -0300

    Update added Entity and Testing

---
+++ src/Shared/Core/ThirdParty/Stripe/Entity/StripeEntity.php
@@ -0,0 +1,163 @@
+<?php namespace PHPAssists\Shared\Core\ThirdParty\Stripe\Entity;
+/**
+ * PHPAssists Stripe API
+ *
+ * This class defines the possible Stripe API algo for the PHPAssists Stripe API.
+ *
+ * @link       https://hexome.com.ar
+ * @since      0.0.5
+ *
+ * @package    PHPAssists
+ * @subpackage PHPAssists\Shared\Core\ThirdParty\Stripe
+ *
+ * @author     Hexome Cloud <hi@hexome.cloud>
+ */
+
+class StripeEntity {
+    private ?string $stripeApiKey;
+    public function __construct(?string $stripeApiKey) {
+        $this->setKeySecret((string) $stripeApiKey);
+    }
+
+    private function getKeySecret() : ? string {
+        return $this->stripeApiKey;
+    }
+
+    private function setKeySecret(?string $stripeApiKey) : void {
+        $this->stripeApiKey = $stripeApiKey;
+    }
+
+    function verifyIdInCSV($paymentIntentId, ?string $filePath) : bool {
+        // Open the file in read mode
+        $file = fopen($filePath, 'r');
+
+        // Iterate over each line in the CSV file
+        while (($row = fgetcsv($file)) !== false) {
+            // Compare the Payment Intent ID with the value of the first column in each row
+            if ($row[0] === $paymentIntentId) {
+                fclose($file);
+                return true; // The Payment Intent ID was found
+            }
+        }
+
+        fclose($file);
+        return false; // The Payment Intent ID was not found
+    }
+
+    function storeJson($json) {
+        // Convert the JSON to a PHP object
+        $object = json_decode($json);
+
+        // Iterate over the object's fields
+        foreach ($object as $field => $value) {
+            // Store the field in the object
+            $object->{$field} = $value;
+        }
+
+        // Return the object
+        return $object;
+    }
+
+
+    function searchCustomer(?string $filePath,?string $customer_id) {
+        // Open the file in read mode
+        $file = fopen($filePath, 'r');
+
+        // Iterate over the lines in the file
+        while (($line = fgets($file)) !== false) {
+            // Split the line into fields
+            $fields = explode(',', $line);
+
+            // Check if the customer field matches
+            if ($fields[3] == $customer_id) {
+                // Return the found fields
+                return $fields;
+            }
+        }
+
+        // Close the file
+        fclose($file);
+
+        // Return null if the customer field is not found
+        return null;
+    }
+
+
+    function verifyExistenceFields(?string $filePath,?string $customer_id) {
+        // Open the file in read mode
+        $file = fopen($filePath, 'r');
+
+        // Define a variable to check the existence of the fields
+        $existeCampos = false;
+
+        // Iterate over each line in the CSV file
+        while (($row = fgetcsv($file)) !== false) {
+            // Check if the line contains 'succeeded' and 'cus_'
+            if (strpos($row[0], 'succeeded') !== false && strpos($row[2], $customer_id) === 0) {
+                $existeCampos = true;
+                break; // If found, stop the search
+            }
+        }
+
+        fclose($file);
+
+        return $existeCampos;
+    }
+
+
+    function storePaymentInCSV($paymentIntentJSON, ?string $filePath) : bool {
+        if (isset($paymentIntentJSON) &&
+        isset($paymentIntentJSON->status) &&
+        isset($paymentIntentJSON->id) &&
+        isset($paymentIntentJSON->customer) && isset($filePath)):
+            $status = $paymentIntentJSON->status;
+            $paymentId = $paymentIntentJSON->id;
+            $customerId = $paymentIntentJSON->customer;
+            $foundFields = [
+                $status,
+                $paymentId,
+                $customerId,
+            ];
+
+            if(!verifyExistenceFields($filePath,$customerId)) {
+                // Open the file in write mode (if it does not exist, it creates it)
+                $file = fopen($filePath, 'a');
+
+                // Create a row for the CSV with the details of the Payment Intent
+                $row = (array) $foundFields;
+
+                // Write the row to the CSV file
+                fputcsv($file, $row);
+                return true;
+                // Close the file
+                fclose($file);
+            } 
+        endif;
+        return false;
+    }
+
+    function compareWithPaymentIntents($fileName) : void
+    {
+        $file = fopen($fileName, 'r'); 
+
+        $stripeApiKey = $this->getKeySecret();
+        \Stripe\Stripe::setApiKey($stripeApiKey);
+
+        $paymentIntents = \Stripe\PaymentIntent::all(['limit' => 100]); 
+
+        while (($data = fgetcsv($file)) !== false) {
+            $customer_id_file = $data[0]; 
+            $email = $data[2]; 
+
+            foreach ($paymentIntents as $paymentIntent) {
+                $customer_id = $paymentIntent->customer;
+                if ($customer_id === $customer_id_file ) {
+                    $filePayments = "payments.txt";
+                    storePaymentInCSV($paymentIntent, $filePayments);    
+                }
+            }
+        }
+
+        fclose($file);
+    }
+}
---
+++ test/Shared/Core/ThirdParty/Stripe/Entity/StripeEntityTest.php
@@ -0,0 +1,70 @@
+<?php
+namespace PHPAssistsTest\Shared\Core\ThirdParty\Stripe\Entity;
+/**
+ * PHPAssists Stripe API Test
+ *
+ * This class defines the possible Functions for the PHPAssists Stripe API Test.
+ *
+ * @link       https://hexome.com.ar
+ * @since      0.0.5
+ *
+ * @package    PHPAssistsTest
+ * @subpackage PHPAssistsTest\Shared\Core\ThirdParty\Stripe\Entity
+ *
+ * @author     Hexome Cloud <hi@hexome.cloud>
+ */
+
+use PHPUnit\Framework\TestCase;
+use PHPAssists\Shared\Core\ThirdParty\Stripe\StripeChecker;
+use PHPAssists\Shared\Core\ThirdParty\Stripe\Entity\StripeEntity;
+
+class StripeEntityTest extends TestCase
+{
+    private ? StripeEntity $stripe;
+    private ? StripeChecker $checker;
+    private ?string $stripe_api_key;
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->stripe_api_key = "sk_test_CGGvfNiIPwLXiDwaOfZ3oX6Y";
+        $this->stripe = new StripeEntity($this->stripe_api_key);
+        $this->checker = new StripeChecker();
+    }
+
+    public function testClassExists() : void
+    {
+        $this->assertInstanceOf(StripeChecker::class, $this->checker);
+    }
+
+    public function testVerifyIdInCSV()
+    {
+        // Crear un archivo CSV de prueba para esta prueba
+        $filePath = 'test_file.csv';
+        $paymentIntentId = 'example_payment_id';
+
+        // Contenido de prueba en el archivo CSV
+        $csvContent = [
+            ['example_payment_id', 'info1', 'info2'],
+            ['another_id', 'info3', 'info4'],
+        ];
+
+        // Escribir contenido en el archivo CSV
+        $file = fopen($filePath, 'w');
+        foreach ($csvContent as $fields) {
+            fputcsv($file, $fields);
+        }
+        fclose($file);
+
+        // Crear una instancia de StripeEntity y ejecutar el método a probar
+        $stripeEntity = new StripeEntity($this->stripe_api_key);
+        $result = $stripeEntity->verifyIdInCSV($paymentIntentId, $filePath);
+
+        // Verificar que el resultado sea verdadero ya que $paymentIntentId está en el archivo CSV
+        $this->assertTrue($result);
+
+        // Eliminar el archivo CSV de prueba después de la prueba
+        unlink($filePath);
+    }
+
+    // Agregar más pruebas para otros métodos de la clase StripeEntity según sea necesario
+}
